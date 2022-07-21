d0058e88711ebfb8a5739f45d10c722993f0e5c2
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-07-22 02:10:33 +0700

    feat: update chat

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Chats.css';
 import Chat from './Chat';
+import useAuth from '../hooks/useAuth';
+import { getUser } from '../firebase';
+import UserModel from '../models/UserModel';
+import unknownAvatar from '../assets/unknown-avatar.png';
+import { cyrb53 } from '../utils/utils';
 
 const Chats = () => {
+  const { user } = useAuth();
+  const [users, setUsers] = useState<UserModel[]>([]);
+
+  useEffect(() => {
+    Promise.all((user?.bookmarks ?? []).map((uid) => getUser(uid))).then(
+      setUsers
+    );
+  }, [user]);
+
   return (
     <div className="chats">
-      <Chat
-        name="Labrador"
-        message="Wuff"
-        timestamp="6 mins ago"
-        profilePic="https://gooddoggies.online/wp-content/uploads/2020/06/5-Tips-To-Training-A-Labrador-Puppy-1.jpg"
-      />
-      <Chat
-        name="Shiba Inu"
-        message="Bork"
-        timestamp="1 hr ago"
-        profilePic="https://thehappypuppysite.com/wp-content/uploads/2019/06/Mini-Shiba-Inu-HP-long.jpg"
-      />
-      <Chat
-        name="Corgi"
-        message="Awooo"
-        timestamp="4 hrs ago"
-        profilePic="https://i.pinimg.com/originals/cb/d4/1f/cbd41fb83c06a915a79ed0ab9ca63789.jpg"
-      />
+      {users.map((u) => (
+        <Chat
+          key={cyrb53(u.email)}
+          name={u.name}
+          message="..."
+          timestamp="..."
+          profilePic={u.image ?? unknownAvatar}
+        />
+      ))}
     </div>
   );
 };
