d38acccb03811df98511b35b97157745e77ccd69
Sherly1001 <Sherly1001@users.noreply.github.com>
2022-07-21 23:00:10 +0700

    fix: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,49 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ChatListPage from './pages/ChatListPage/ChatListPage';
 import ChatDetailPage from './pages/ChatDetailPage/ChatDetailPage';
-import {wrapWithLoginRequire} from './components/RequireLogin/RequireLogin';
+import { wrapWithLoginRequire } from './components/RequireLogin/RequireLogin';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
-import useAuth from "./hooks/useAuth";
-import AdminPage from "./pages/AdminPage/AdminPage";
+import useAuth from './hooks/useAuth';
+import AdminPage from './pages/AdminPage/AdminPage';
 
 function App() {
-  const {user} = useAuth()
+  const { user } = useAuth();
 
-  const isAdmin = user?.email === "admin@finder.com"
+  const isAdmin = user?.email === 'admin@finder.com';
 
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          {
-            isAdmin ?
-              <>
-                <Route path="/admin">{wrapWithLoginRequire(<AdminPage/>)}</Route>
-                <Route path="/">
-                  <Redirect to="/admin" />
-                </Route>
-              </>
-              :
-              <>
-                <Route path="/chat/:person">
-                  {wrapWithLoginRequire(<ChatDetailPage/>)}
-                </Route>
-                <Route path="/chat">{wrapWithLoginRequire(<ChatListPage/>)}</Route>
-                <Route path="/profile">{wrapWithLoginRequire(<ProfilePage/>)}</Route>
-                <Route path="/">{wrapWithLoginRequire(<HomePage/>)}</Route>
-                <Route path="/admin">
-                  <Redirect to="/" />
-                </Route>
-              </>
-          }
+          {isAdmin ? (
+            <>
+              <Route path="/admin">{wrapWithLoginRequire(<AdminPage />)}</Route>
+              <Route path="/">
+                <Redirect to="/admin" />
+              </Route>
+            </>
+          ) : (
+            <>
+              <Route path="/chat/:person">
+                {wrapWithLoginRequire(<ChatDetailPage />)}
+              </Route>
+              <Route path="/chat">
+                {wrapWithLoginRequire(<ChatListPage />)}
+              </Route>
+              <Route path="/profile">
+                {wrapWithLoginRequire(<ProfilePage />)}
+              </Route>
+              <Route path="/" exact>
+                {wrapWithLoginRequire(<HomePage />)}
+              </Route>
+              <Route path="/admin">
+                <Redirect to="/" />
+              </Route>
+            </>
+          )}
         </Switch>
       </BrowserRouter>
     </div>
